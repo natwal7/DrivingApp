190ad8ff8577c3d6cb500c30adea5416548f2547
exampleCoderOne <exampleemail3001@gmail.com>
2023-06-06 22:46:34 -0700

    MultiSelectButtons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import { MultiSelectButtons } from './components/MultiSelectButtons';
 
 function App() {
+  const [selectedButtonOne, setSelectedButtonOne] = useState<number>();
+  const [selectedButtonTwo, setSelectedButtonTwo] = useState<number>();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Learning to Drive</p>   
       </header>
+
+      <div className='ControlBar'>
+        <div className="DashButton">
+          <p>Dashboard Button here</p>
+        </div>
+        <div className="HoursButton">
+          <p>Hours Log Button here</p>
+        </div>
+      </div>
+
+      <div className='Timer'>
+        <div className="StartButton">
+          <button>start</button>
+        </div>
+        <div className="StopButton">
+          <button>stop</button>
+        </div>
+        <div className="RunningTime">
+          <p>Running Time here</p>
+        </div>
+        <MultiSelectButtons value={selectedButtonOne} onChange={setSelectedButtonOne}  options={['one', 'three', 'two', 'ten', 'seven', 'apple']}/>
+        <MultiSelectButtons value={selectedButtonTwo} onChange={setSelectedButtonTwo} className={'yellow'} options={['zero', 'two', 'one', 'nine', 'eight', 'pepper']}/>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/MultiSelectButtons.tsx
@@ -0,0 +1,37 @@
+// Takes in props (value, etc.)
+// need to make: value variable, SetValue function, className, options
+// when clicked, changes value (onChange)
+// button w/value of "on" is displayed
+// note: using props, with the variable and function declared wherever it's used, so that the value changes here can affect things outside of it
+
+import { ReactNode } from "react";
+
+function MultiSelectButtons(props:{
+    value:number | undefined,
+    onChange:(value:number)=>void, 
+    className?: string,
+    options: ReactNode[]
+}) {
+    //   props.value
+    
+    // () => 
+
+    return(
+        <div>
+            {props.options.map((option, i) => (
+                <button
+                    key={i}
+                    className={`${props.className ?? ''} ${props.value === i ? 'on' : ''}`} // ?? operator means if the term is null/undefined, set it to first
+                    onClick={() => props.onChange(i)}
+                >{option}</button>
+            ))}
+        </div>
+    )
+
+        // condition ? value if true : value if false
+}
+
+// const [page, setPage] = useState(0);
+// <MultiSelectBUttons value={page} onChange={setPage} />
+
+export {MultiSelectButtons};
